ecbeb426802ae0659ce6875dbc98d3b901965e46
mcmorgan6560@gmail.com <mcmorgan6560@gmail.com>
2022-11-08 00:55:08 +0100

    added projects section and made the form functional

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,21 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwpjIsj55ajORq_kpcoFsEwfXkWzKib-MCNLqytojzycAxSF1sz7jU6LrgxzZ-I4VpR/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
